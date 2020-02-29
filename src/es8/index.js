4e98098d2a46edd078ed8681450a435fec3a5475
Alexis Araujo <ala1289@gmail.com>
2020-02-28 23:03:45 -0600

    ¿Qué se implemento en ES8?

---
+++ src/es8/index.js
@@ -0,0 +1,12 @@
+// Object.entries
+const data = {
+  frontend: 'Juan',
+  backend: 'Carlos',
+  design: 'Ana',
+};
+const entries = Object.entries(data);
+const key = Object.keys(data);
+const values = Object.values(data);
+console.log(entries);
+console.log(key);
+console.log(values);

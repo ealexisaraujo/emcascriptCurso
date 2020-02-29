c5c23a3e91b67a3f07f3f13aae1f0d5e2eee8363
Alexis Araujo <ala1289@gmail.com>
2020-02-28 23:17:37 -0600

    async await

--- src/es8/index.js
+++ src/es8/index.js
@@ -10,3 +10,28 @@ const values = Object.values(data);
 console.log(entries);
 console.log(key);
 console.log(values);
+
+//=> async await
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    true
+      ? setTimeout(() => resolve('Hello Juan Carlos'), 3000)
+      : reject(new Error('Test Error'));
+  });
+};
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  console.log(hello);
+};
+helloAsync();
+
+// asi se ejecuta correctamente una promesa
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello);
+  } catch (error) {
+    console.log(error);
+  }
+};
+anotherFunction();
